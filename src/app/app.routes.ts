628cbc35656613a2473ee378a15994e5752c177f
Javi Caiola <javiteen1250@gmail.com>
2024-05-09 23:22:43 -0300

    routing inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PaginaInicioComponent } from './components/mains/pagina-inicio/pagina-inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'inicio',pathMatch:'full'},
+    {path:'inicio', component:PaginaInicioComponent}
+];
