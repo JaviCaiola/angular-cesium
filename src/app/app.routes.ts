4caf3c8c6e58c0fc12d6c99d160cfa54f99401f9
Javi Caiola <javiteen1250@gmail.com>
2024-05-11 22:38:44 -0300

    inicializando la configuracion de cesium

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PaginaInicioComponent } from './components/mains/pagina-inicio/pagina-inicio.component';
+import { PaginaMapaComponent } from './components/mains/pagina-mapa/pagina-mapa.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'inicio',pathMatch:'full'},
-    {path:'inicio', component:PaginaInicioComponent}
+    {path:'inicio', component:PaginaInicioComponent},
+    {path:'visor-mapa', component:PaginaMapaComponent}
 ];
---
+++ src/app/cesium-map.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCesiumMap]',
+  standalone: true
+})
+export class CesiumMapDirective {
+
+  constructor() { }
+
+}
