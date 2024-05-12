0ffcc0025b8f36c62d42114a0e14751c4d610f00
Javi Caiola <javiteen1250@gmail.com>
2024-05-12 15:52:25 -0300

    commit 15-52

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularcesium';
+  title = 'Prueba de Compatibilidad Angular - Cesium';
 }
